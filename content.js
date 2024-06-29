7162a4d37f42f8b31fb1a3e36b83b8a90dd61c07
shalomHolam <130912322+shalomHolam@users.noreply.github.com>
2024-06-29 21:57:20 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,12 @@
-function main() {    
-    let newHtml = 
-    `<li>
-        <div class="p-navEl " data-has-children="true" style="user-select: none; pointer-events: auto;">
-            <a id="ranom-content-menu" data-random-content-type="any" class="random-content p-navEl-link p-navEl-link--splitMenu ">תוכן אקראי</a>
-            <a data-xf-key="3" data-xf-click="menu" data-menu-pos-ref="< .p-navEl" class="p-navEl-splitTrigger" role="button" tabindex="0" aria-label="בחר באפשרות 'הרחב'" aria-expanded="false" aria-haspopup="true"></a>
+function main() {
+    let newHtml = `
+    <li data-line-height="0px">
+        <div class="p-navEl" data-has-children="true" data-font-size="13" data-font-size-type="px" data-line-height="18.2px">
+            <a id="ranom-content-menu" data-random-content-type="any" class="random-content p-navEl-link p-navEl-link--splitMenu" data-nav-id="randomContent">
+                תוכן אקראי
+                ${shouldIncludeNewBadge() ? '<span class="new-badge">חדש</span>' : ''}
+            </a>
+            <a data-xf-key="3" data-xf-click="menu" data-menu-pos-ref="< .p-navEl" class="p-navEl-splitTrigger" role="button" tabindex="0" aria-label="בחר באפשרות \'הרחב\'" aria-expanded="false" aria-haspopup="true"></a>
             <div class="menu menu--structural" data-menu="menu" aria-hidden="true">
                 <div class="menu-content">
                     <a data-random-content-type="threads" class="random-content menu-linkRow u-indentDepth0 js-offCanvasCopy">נושא</a>
@@ -16,10 +19,25 @@ function main() {
                 </div>
             </div>
         </div>
-    </li>`;
-    let ul = document.querySelector("div.p-nav-scroller.hScroller > div > ul");
-    if(ul) {
-        ul.innerHTML += newHtml;
+    </li>
+    ${shouldIncludeNewBadge() ? `
+    <style id="random-content-menu-style">
+    .new-badge {
+        background-color: #c84448; /* צבע הרקע לסימון */
+        color: #ffffff; /* צבע הטקסט */
+        font-size: 10px; /* גודל הטקסט */
+        padding: 2px 4px; /* ריווח פנימי */
+        border-radius: 3px; /* פינות מעוגלות */
+        margin-left: 5px; /* רווח מצד שמאל */
+        vertical-align: super; /* מיקום מעל לטקסט */
+    }
+    </style>` : ''}
+    `;
+
+    let previousItem = document.querySelector("#top > div.p-sectionLinks > div > div > ul > li:nth-child(2)");
+
+    if (previousItem) {
+        previousItem.insertAdjacentHTML('afterend', newHtml);
     }
 
     let url = document.location.href;
@@ -34,11 +52,11 @@ function main() {
 
     function randomLink(type) {
         if (type === "any") {
-            let RantypeNum = Math.floor(Math.random() * 5);
+            let RantypeNum = Math.floor(Math.random() * Object.keys(contentTypes).length);
             type = Object.keys(contentTypes)[RantypeNum];
         }
-        let ranContentNum = Math.floor((Math.random() * contentTypes[type]));
-        return `https://tora-forum.co.il/${type}/${ranContentNum}?random=${1}`;
+        let = Math.floor((Math.random() * contentTypes[type]));
+        return `https://tora-forum.co.il/${type}/${ranContentNum}?random=1`;
     }
 
     function setHref(element) {
@@ -48,7 +66,7 @@ function main() {
 
     function newRandomContent() {
         document.title = "מחפש תוכן אקראי אחר...";
-        let contentType = url.match(/forums|threads|posts|members|ams|attachments/g)[0];
+        let contentType = url.match(/forums|threads|posts|members|ams|attachments/)[0];
         document.location = randomLink(contentType);
     }
 
@@ -60,7 +78,7 @@ function main() {
 
         if (url.includes("member")) {
             let elements = document.querySelectorAll("dl > dd > a");
-            if (elements.length > 0 && elements[0].innerText < 3) {
+            if (elements.length > 0 && parseInt(elements[0].innerText) < 3) {
                 newRandomContent();
                 return;
             }
@@ -74,16 +92,66 @@ function main() {
     const observer = new MutationObserver(checkTitleAndURL);
     observer.observe(document.querySelector('title'), { childList: true });
 
-    $(".random-content").each((x, e) => setHref(e));
+    document.querySelectorAll(".random-content").forEach(e => setHref(e));
 
     document.querySelectorAll(".random-content").forEach(e => {
         e.addEventListener("mousedown", (event) => {
-            if(event.button === 1) {
+            if (event.button === 1) {
                 setHref(e);
             }
         });
     });
 
-}
+    document.getElementById('ranom-content-menu').addEventListener('click', updateUsageCount);
 
-window.addEventListener('load', main);
+
+    function updateUsageCount() {
+        let usageCount = getCookie('randomContentUsageCount') || 0;
+
+        // המרת הערך למספר והוספת שימוש
+        usageCount = parseInt(usageCount) + 1;
+
+        // עדכן את הערך בעוגיה
+        setCookie('randomContentUsageCount', usageCount, 30); // שמירה ל-30 ימים
+
+        // בדוק אם המשתמש כבר השתמש 5 פעמים או יותר
+        if (usageCount >= 5) {
+            hideNewBadge();
+        }
+    }
+
+    function getCookie(name) {
+        let nameEQ = name + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    function setCookie(name, value, days) {
+        let expires = "";
+        if (days) {
+            let date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    function shouldIncludeNewBadge() {
+        let usageCount = getCookie('randomContentUsageCount') || 0;
+        return usageCount < 5;
+    }
+
+    function hideNewBadge() {
+        // פונקציה להסתרת הסימון 'חדש'
+        let newBadge = document.querySelector('.new-badge');
+        if (newBadge) {
+            newBadge.style.display = 'none';
+        }
+    }
+}
+addEventListener('load', main);
